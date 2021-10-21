6d16d7b537747f23b2b5b51b770b8a7003741d3e
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 22:30:16 +0000

    implement update and middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -25,19 +25,22 @@ bookRouter.route('/books')
     res.status(201).json(book);
   });
 
-bookRouter.route('/books/:bookId').get((req, res) => {
+  bookRouter.use('/books/:bookId',(req, res, next) => {
   Book.findById(req.params.bookId, (err, book) => {
     if (err) {
       return res.send(err);
     }
-    return res.json(book);
-  });
-})
-.put((req, res)=>{
-  Book.findById(req.params.bookId,(err,book)=>{
-    if(err){
-      res.send(err);
+    if(book){
+      req.book = book;
+      return next()
     }
+    return res.sendStatus(404);
+  });
+  })
+
+bookRouter.route('/books/:bookId').get((req, res) => res.json(req.book))
+.put((req, res)=>{
+    const {book} = req;
     book.title = req.body.title;
     book.author = req.body.author;
     book.gener = req.body.gener;
@@ -45,7 +48,6 @@ bookRouter.route('/books/:bookId').get((req, res) => {
     book.save();
     return res.json(book);
   });
-});
 
 
 return bookRouter;
