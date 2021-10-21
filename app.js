d47dc3a7dbc50bc65a06e5a109a5e2c30421c51b
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 14:57:33 +0000

    add mongodb and mongoose connections

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const { response } = require('express');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+const db = mongoose.connect('mongodb://localhost/books');
 const port = process.env.PORT || 8976;
+
+const Book = require('./models/bookModel');
 
 const bookRouter = express.Router();
 
@@ -11,11 +15,14 @@ const bookRouter = express.Router();
 
 bookRouter.route('/books')
   .get((req, res)=>{
-    const response = { hello: 'This is my API' };
-
-    res.json(response);
+    Book.find((err, books) => {
+      if(err){
+       return  res.send(err);
+      }
+      return res.json(books);
+    });
   });
-
+    
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
---
+++ booksJson.js
@@ -0,0 +1,20 @@
+db.books.insert([
+  {
+    title: "War and Peace",
+    genre: "Historical Fiction",
+    author: "Lev Niko",
+    read: false,
+  },
+  {
+    title: "Awekening the Giant Within",
+    genre: "Self Help",
+    author: "Anthony Roberts",
+    read: false,
+  },
+  {
+    title: "Rich Dad, Poor Dad",
+    genre: "Self Help",
+    author: "Robert Kayosaki",
+    read: true,
+  }
+]);
