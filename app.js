e036196fdf0e61acad3e9f12b8205f8ccfcc59df
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 18:40:06 +0000

    implement saving book in mongodb

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const { response } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/books');
 const port = process.env.PORT || 8976;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const Book = require('./models/bookModel');
 
@@ -12,7 +15,8 @@ const bookRouter = express.Router();
 
 //routes
 
-bookRouter.route('/books').get((req, res) => {
+bookRouter.route('/books')
+.get((req, res) => {
   const  query  = {};
 
   if(req.query.genre){
@@ -25,32 +29,23 @@ bookRouter.route('/books').get((req, res) => {
     }
     return res.json(books);
   });
-});
-
-
-bookRouter.route('/books').get((req, res) => {
-  const query = {};
-
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(books);
-  });
-});
+})
+.post((req, res) =>{
+  const book = new Book(req.body);
+  console.log(book);
+  book.save()
+  res.status(201).json(book);
+})
+;
 
 bookRouter.route('/books/:bookId').get((req, res)=>{
 
- Book.findById(req.params.bookId,(err,book)=>{
-   if(err){
-     return res.send(err);
-   }
-   return res.json(book);
- })
+Book.findById(req.params.bookId,(err,book)=>{
+  if(err){
+    return res.send(err);
+  }
+  return res.json(book);
+})
 
 })
 
--- booksJson.js
+++ booksJson.js
@@ -1,20 +1,23 @@
 db.books.insert([
   {
-    title: "War and Peace",
-    genre: "Historical Fiction",
-    author: "Lev Niko",
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Niko',
     read: false,
   },
+
   {
-    title: "Awekening the Giant Within",
-    genre: "Self Help",
-    author: "Anthony Roberts",
+    title: 'Awekening the Giant Within',
+    genre: 'Self Help',
+    author: 'Anthony Roberts',
     read: false,
   },
+
   {
-    title: "Rich Dad, Poor Dad",
-    genre: "Self Help",
-    author: "Robert Kayosaki",
+    title: 'Rich Dad, Poor Dad',
+    genre: 'Self Help',
+    author: 'Robert Kayosaki',
     read: true,
   }
+  
 ]);
