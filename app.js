5aec3cacd4046a479313662b21db2eb927e535d4
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 17:10:42 +0000

    add filtering by query params

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const { response } = require('express');
-const express = require('express');
-const mongoose = require('mongoose');
+const { response } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/books');
+const db = mongoose.connect("mongodb://localhost/books");
 const port = process.env.PORT || 8976;
 
-const Book = require('./models/bookModel');
+const Book = require("./models/bookModel");
 
 const bookRouter = express.Router();
 
-
 //routes
 
-bookRouter.route('/books')
-  .get((req, res)=>{
-    Book.find((err, books) => {
-      if(err){
-       return  res.send(err);
-      }
-      return res.json(books);
-    });
+bookRouter.route("/books").get((req, res) => {
+  const { query } = req;
+  Book.find(query,(err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
   });
-    
-app.use('/api', bookRouter);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello APi');
+app.use("/api", bookRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello APi");
 });
 
 app.listen(port, () => {
