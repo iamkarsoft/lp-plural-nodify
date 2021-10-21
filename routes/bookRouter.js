1c2458b068fe77bf07058477566a76c69f2ea331
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 22:05:02 +0000

    implement updating resource

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,8 +1,9 @@
+/* eslint disabled no-params-reassign */
 const express = require('express');
 
 function route(Book){
   const bookRouter = express.Router();
-bookRouter.route("/books")
+bookRouter.route('/books')
   .get((req, res) => {
     const query = {};
 
@@ -24,11 +25,24 @@ bookRouter.route("/books")
     res.status(201).json(book);
   });
 
-bookRouter.route("/books/:bookId").get((req, res) => {
+bookRouter.route('/books/:bookId').get((req, res) => {
   Book.findById(req.params.bookId, (err, book) => {
     if (err) {
       return res.send(err);
     }
+    return res.json(book);
+  });
+})
+.put((req, res)=>{
+  Book.findById(req.params.bookId,(err,book)=>{
+    if(err){
+      res.send(err);
+    }
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.gener = req.body.gener;
+    book.read = req.body.read;
+    book.save();
     return res.json(book);
   });
 });
