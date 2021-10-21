1762a231a5b10e9d2765c7ba34a92c22e5d71931
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 01:02:47 +0000

    set up express router and api route

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 8976;
 
+const bookRouter = express.Router();
+
+
+//routes
+
+bookRouter.route('/books')
+  .get((req, res)=>{
+    const response = { hello: 'This is my API' };
+
+    res.json(response);
+  });
+
+app.use('/api', bookRouter);
+
 app.get('/', (req, res) => {
   res.send('Hello APi');
 });
