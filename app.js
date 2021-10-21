b64b167564588d9cc9053a60de11791cf612788e
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-10-21 17:49:04 +0000

    query for single resource

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const { response } = require("express");
-const express = require("express");
-const mongoose = require("mongoose");
+const { response } = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const db = mongoose.connect("mongodb://localhost/books");
+const db = mongoose.connect('mongodb://localhost/books');
 const port = process.env.PORT || 8976;
 
-const Book = require("./models/bookModel");
+const Book = require('./models/bookModel');
 
 const bookRouter = express.Router();
 
 //routes
 
-bookRouter.route("/books").get((req, res) => {
+bookRouter.route('/books').get((req, res) => {
   const  query  = {};
 
   if(req.query.genre){
@@ -27,10 +27,37 @@ bookRouter.route("/books").get((req, res) => {
   });
 });
 
-app.use("/api", bookRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello APi");
+bookRouter.route('/books').get((req, res) => {
+  const query = {};
+
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+});
+
+bookRouter.route('/books/:bookId').get((req, res)=>{
+
+ Book.findById(req.params.bookId,(err,book)=>{
+   if(err){
+     return res.send(err);
+   }
+   return res.json(book);
+ })
+
+})
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello APi');
 });
 
 app.listen(port, () => {
